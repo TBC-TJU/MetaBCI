3758874b048411718ef3cf9d2f575575568e41d5
swolf <swolfforever@gmail.com>
2021-03-20 20:39:48 +0800

    update readme;

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 import setuptools
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+with open("README.md", "rb", errors="ignore") as fh:
+    long_description = fh.read().decode("utf-8")
 
 setuptools.setup(
     name="brainda",
