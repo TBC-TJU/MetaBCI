2bfb439f5a5f094475c85a0dbd8686150ddf8803
JIeLu-Wu <72449643+JIeLu-Wu@users.noreply.github.com>
2023-01-12 14:45:57 +0800

    AVEP Update

--- demos/brainstim_demos/stim_demo.py
+++ demos/brainstim_demos/stim_demo.py
@@ -1,12 +1,15 @@
 from psychopy import monitors
 import numpy as np
-from metabci.brainstim.paradigm import SSVEP,P300,MI,paradigm
+from metabci.brainstim.paradigm import SSVEP,P300,MI,AVEP,paradigm
 from metabci.brainstim.framework import Experiment
+
+
+
 
 if __name__=='__main__':
     mon = monitors.Monitor(
             name='primary_monitor', 
-            width=59.6, distance=60,    # width 显示器尺寸cm; distance 受试者与显示器间的距离 
+            width=40, distance=60,    # width 显示器尺寸cm; distance 受试者与显示器间的距离
             verbose=False
         )
     mon.setSizePix([1920, 1080])        # 显示器的分辨率
@@ -51,7 +54,7 @@ if __name__=='__main__':
 
     bg_color = np.array([-1, -1, -1])                           # 背景颜色
     display_time = 1                                            # 范式开始1s的warm时长
-    index_time = 0.5                                            # 提示时长，转移视线
+    index_time = 1                                          # 提示时长，转移视线
     rest_time = 0.5                                             # 提示后的休息时长
     response_time = 1                                           # 在线反馈    
     port_addr = None #  0xdefc                                  # 采集主机端口
@@ -60,8 +63,53 @@ if __name__=='__main__':
     online = False # True                                       # 在线实验的标志
     ex.register_paradigm('basic SSVEP', paradigm, VSObject=basic_ssvep, bg_color=bg_color, display_time=display_time,
                          index_time=index_time, rest_time=rest_time, response_time=response_time, port_addr=port_addr, nrep=nrep, 
-                         pdim='ssvep', lsl_source_id=lsl_source_id, online=online, device_type='Neuracle')
+                         pdim='ssvep', lsl_source_id=lsl_source_id, online=online)
     
+    '''
+    AVEP
+    '''
+    n_elements, rows, columns = 32, 4, 8  # n_elements 指令数量;  rows 行;  columns 列
+    stim_length, stim_width = 3, 3  # avep刺激点的尺寸
+    tex_height = 25 # avep指令的大小
+    stim_color, tex_color = [0.7, 0.7, 0.7], [1, 1, 1]  # 指令的颜色，文字的颜色
+    fps = 60  # 屏幕刷新率
+    stim_time = 1  # 刺激时长
+    stim_opacities = 1  # 刺激对比度
+    freqs = np.ones((n_elements)) * 10 # 指令的频率
+    # phases = np.array([i * 0.35 % 2 for i in range(n_elements)])  # 指令的相位
+    stim_num = 2
+    avep = AVEP(win=win,dot_shape='cluster')
+    sequence = [avep.num2bin_ary(i,5) for i in range(n_elements)]
+    # sequence = [[1,2,3,4] for i in range(n_elements)]
+    if len(sequence) != n_elements:
+        raise Exception('Incorrect spatial code amount!')
+
+
+    avep.tex_height = tex_height
+    avep.config_pos(n_elements=n_elements, rows=rows, columns=columns, stim_length=stim_length, stim_width=stim_width)
+    avep.config_color(refresh_rate=fps, stim_time=stim_time, stimtype='sinusoid',
+                      stim_color=stim_color,sequence=sequence,
+            stim_opacities=stim_opacities, freqs=freqs, stim_num=stim_num)
+
+    avep.config_text(symbol_height = tex_height, tex_color=tex_color)
+    avep.config_index(index_height=40)
+    avep.config_response()
+
+    bg_color = np.array([-1, -1, -1])                           # 背景颜色
+    display_time = 1                                            # 范式开始1s的warm时长
+    index_time = 0.5                                            # 提示时长，转移视线
+    rest_time = 0.5                                             # 提示后的休息时长
+    response_time = 1                                           # 在线反馈
+    port_addr = None #  0xdefc                                  # 采集主机端口
+    nrep = 1                                                    # block数目
+    lsl_source_id = 'meta_online_worker' # None                 # source id
+    online = False # True                                       # 在线实验的标志
+    ex.register_paradigm('avep', paradigm, VSObject=avep, bg_color=bg_color, display_time=display_time,
+                         index_time=index_time, rest_time=rest_time, response_time=response_time, port_addr=port_addr, nrep=nrep,
+                         pdim='avep', lsl_source_id=lsl_source_id, online=online)
+
+
+
     '''
     P300
     '''
