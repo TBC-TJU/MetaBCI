6dfcc4d7a7009781bc0ced0e34ec85779aa3a178
JIeLu-Wu <72449643+JIeLu-Wu@users.noreply.github.com>
2023-03-27 17:54:41 +0800

    Update base.py
    Format Adjustment.

--- metabci/brainda/algorithms/decomposition/base.py
+++ metabci/brainda/algorithms/decomposition/base.py
@@ -13,6 +13,7 @@ from sklearn.base import BaseEstimator, TransformerMixin, clone
 from metabci.brainda.datasets.base import BaseTimeEncodingDataset
 import mne
 
+
 def robust_pattern(W: ndarray, Cx: ndarray, Cs: ndarray) -> ndarray:
     """Transform spatial filters to spatial patterns based on paper [1]_.
 
